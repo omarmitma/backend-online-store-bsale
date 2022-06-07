2eb1456120357d2d1c463690cffe679ac471d4ce
omarmitma <omarmitma123@gmail.com>
2022-06-07 10:05:37 -0500

    commit

--- src/main/java/com/gm/repository/ProductRepository.java
+++ src/main/java/com/gm/repository/ProductRepository.java
@@ -12,20 +12,20 @@ public interface ProductRepository extends JpaRepository<product, Integer>{
     
     
     
-    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND  p.category = :category AND p.name LIKE :name%", nativeQuery = true)
+    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND  p.category = :category AND p.name LIKE %:name%", nativeQuery = true)
     List<product> getAmountOfFilteredProducts(int category, String name);
     
     
     
-    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE :name% ORDER BY p.name ASC LIMIT :limit OFFSET :offset", nativeQuery = true)
+    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE %:name% ORDER BY p.name ASC LIMIT :limit OFFSET :offset", nativeQuery = true)
     List<product> filterProductsByNameASCWithLimit(int category, String name, int limit, int offset);
     
-    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE :name% ORDER BY p.name DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
+    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE %:name% ORDER BY p.name DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
     List<product> filterProductsByNameDESCWithLimit(int category, String name, int limit, int offset);
     
-    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE :name% ORDER BY p.price ASC LIMIT :limit OFFSET :offset", nativeQuery = true)
+    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE %:name% ORDER BY p.price ASC LIMIT :limit OFFSET :offset", nativeQuery = true)
     List<product> filterProductsByPrecioASCWithLimit(int category, String name, int limit, int offset);
     
-    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE :name% ORDER BY p.price DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
+    @Query(value="SELECT * from product p WHERE p.url_image <> '' AND p.category = :category AND p.name LIKE %:name% ORDER BY p.price DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
     List<product> filterProductsByPrecioDESCWithLimit(int category, String name, int limit, int offset);
 }
